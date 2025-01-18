c2774bd430d183364774f9e6b64e48ab2517f435
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 17:16:26 -0300

    Refactor shop page layout to integrate HeroShop, ShopByPet, FiltersAndCards, and Banners components for improved structure and user experience

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -5,5 +5,10 @@ definePageMeta({
 </script>
 
 <template>
-  <Button label="Shop page" id="shop-page-button" />
+  <main class="flex flex-col flex-wrap h-full mx-auto min-w-max min-h-[94vh] w-full items-center relative">
+    <HeroShop />
+    <ShopByPet />
+    <FiltersAndCards />
+    <Banners />
+  </main>
 </template>
