989efb4543878bc4433996787177a682bad9c6a0
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 19:11:19 -0300

    Implement Featured Products section with dynamic product display. Added a script to manage product data and updated the template to include a grid layout for showcasing products with images, names, and prices, enhancing the overall home page experience.

--- src/blocks/sections/home/FeaturedProducts.vue
+++ src/blocks/sections/home/FeaturedProducts.vue
@@ -1,8 +1,43 @@
-<template>
-  <Button
-    class="w-max mx-auto h-max my-auto"
-    label="Featured Products"
-    id="featured-products-button"
-  />
-</template>
+<script setup lang="ts">
+import backgroundGradientOrange from "@/assets/images/background-gradient-orange.webp";
+import backgroundGradientOrangeMini from "@/assets/images/background-gradient-orange-mini.webp";
+import katherineMcadoo from "@/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp";
 
+const products = [
+  {
+    imageSrc: backgroundGradientOrange,
+    name: "Premium Dog Food",
+    price: "$18.99",
+  },
+  {
+    imageSrc: katherineMcadoo,
+    name: "Premium Cat Food",
+    price: "$18.99",
+  },
+  {
+    imageSrc: backgroundGradientOrangeMini,
+    name: "Premium Dog Food",
+    price: "$18.99",
+  },
+];
+</script>
+
+<template>
+  <div class="container mx-auto my-8">
+    <Button
+      class="w-max mx-auto h-max my-auto"
+      label="Featured Products"
+      id="featured-products-button"
+    />
+    <h2 class="text-2xl font-bold text-center my-4">Featured Products</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <FeatureCard
+        v-for="(product, index) in products"
+        :key="index"
+        :imageSrc="product.imageSrc"
+        :productName="product.name"
+        :productPrice="product.price"
+      />
+    </div>
+  </div>
+</template>
