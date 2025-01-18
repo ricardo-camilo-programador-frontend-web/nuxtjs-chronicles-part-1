94457da25ec301c07230be8e8a806caa1a0ff7c7
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 11:02:36 -0300

    Implement Image component with TypeScript support
    - Created a new Image.vue component that accepts various props including src, alt, class, width, height, and optional attributes for loading behavior and accessibility.
    - Utilized TypeScript for prop definitions to enhance type safety and improve code maintainability.
    - The component is designed to streamline image rendering with customizable attributes, contributing to better performance and accessibility in the application.

---
+++ src/components/Image.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+interface Props {
+  src: string
+  alt: string
+  class: string
+  width: number
+  height: number
+  ariaHidden?: boolean
+  title?: string
+  loading?: 'eager' | 'lazy'
+  ariaLabel?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  loading: 'lazy',
+  ariaHidden: true,
+})
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :class="class"
+    :title="title"
+    :aria-hidden="ariaHidden"
+    :loading="loading"
+    :aria-label="ariaLabel"
+    :width="width"
+    :height="height"
+  />
+</template>
