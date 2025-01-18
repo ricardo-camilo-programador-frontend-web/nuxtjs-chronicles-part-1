01feb7a50e2600f000ae36fa6c281ecdbf9c1d7e
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-17 23:04:06 -0300

    Add useScroll composable for scroll position tracking
    - Introduced a new composable function `useScroll` to manage scroll position and direction.
    - Implemented reactive properties to track if the user is scrolling down and if the page is scrolled.
    - Added event listeners for scroll events on component mount and cleanup on unmount.

---
+++ src/composables/useScroll.ts
@@ -0,0 +1,26 @@
+export function useScroll() {
+  const lastScrollPosition = ref(0)
+  const isScrollingDown = ref(false)
+  const isScrolled = ref(false)
+
+  const handleScroll = () => {
+    const currentScrollPosition = window.scrollY
+    isScrolled.value = currentScrollPosition > 0
+
+    isScrollingDown.value = currentScrollPosition > lastScrollPosition.value
+    lastScrollPosition.value = currentScrollPosition
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', handleScroll)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll)
+  })
+
+  return {
+    isScrollingDown,
+    isScrolled
+  }
+}
