fe077f762e213b48248378fa4796708dd5683032
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 19:20:43 -0300

    Update Button component hover color for improved visual feedback. Changed hover background color from gray to orange to enhance user interaction experience.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -52,7 +52,7 @@ initializeRippleEffect()
     :type="type"
     :disabled="disabled"
     :title="title"
-    class="rounded-md font-bold text-xs tracking-widest p-4 ease-in-out duration-1350 !transition-all hover:bg-gray-300 disabled:opacity-25 flex items-center justify-center"
+    class="rounded-md font-bold text-xs tracking-widest p-4 ease-in-out duration-1350 !transition-all hover:bg-orange-300 disabled:opacity-25 flex items-center justify-center"
   >
     <Icon
       v-if="icon"
