c03c587b8c816f1539b10e28449a9fdfa40c6019
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 17:18:27 -0300

    Refactor About page layout to integrate multiple components including HeroAbout, AboutOurStore, InfoBlock, OurTeam, Testimonials, Video, and FollowOurInstagram for enhanced structure and user engagement

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -5,9 +5,13 @@ definePageMeta({
 </script>
 
 <template>
-  <Button
-    class="bg-red-500 w-max mx-auto h-max my-auto"
-    label="About page"
-    id="about-page-button"
-  />
+  <main class="flex flex-col flex-wrap h-full mx-auto min-w-max min-h-[94vh] w-full items-center relative">
+    <HeroAbout />
+    <AboutOurStore />
+    <InfoBlock />
+    <OurTeam />
+    <Testimonials />
+    <Video />
+    <FollowOurInstagram />
+  </main>
 </template>
