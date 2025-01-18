3daddaf54bdf41e056489e885689a09a64d95bc4
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-18 10:35:45 -0300

    Add NProgress for page loading indicators</message>
    <message>Updated package.json to include NProgress as a dependency and modified app.vue to implement loading indicators during page transitions.

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+provideHeadlessUseId(() => useId())
+
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook('page:start', () => {
+  Nprogress.start()
+})
+
+nuxtApp.hook('page:finish', () => {
+  Nprogress.done()
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
