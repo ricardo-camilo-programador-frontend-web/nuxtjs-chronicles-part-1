3fdbf3de18099f452e3953bf2928a25cf8deb202
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-19 04:36:40 -0300

    Add ProductCard component for product display and interaction
    - Introduced a new ProductCard component to showcase individual product details including image, name, price, and favorite status.
    - Implemented TypeScript for props definition, ensuring type safety.
    - Enhanced user interaction with a button to toggle favorite status, improving user engagement.

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface Props {
+  product: Product;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <article class="bg-gray-200 rounded-lg transition-shadow hover:shadow-lg">
+    <div class="relative aspect-square mb-4">
+      <Image
+        :src="product.imageSrc"
+        :alt="product.altText"
+        class="w-full h-full object-cover rounded-lg  p-4"
+        loading="lazy"
+        width="100"
+        height="100"
+      />
+    </div>
+
+    <div class="space-y-2 bg-white px-4 py-2 rounded-b-lg">
+      <div class="flex items-center justify-between w-full">
+        <h3 class="font-medium text-lg">{{ product.name }}</h3>
+
+        <Button
+          class="p-2"
+          icon-style="text-red-500"
+          :icon="product.isFavorite ? 'i-heroicons-heart-solid' : 'i-heroicons-heart'"
+          :aria-label="
+            product.isFavorite ? 'Remove from favorites' : 'Add to favorites'
+          "
+        />
+      </div>
+      <p class="text-gray-900 font-bold">
+        {{ product.currency }} {{ product.price.toFixed(2) }}
+      </p>
+    </div>
+  </article>
+</template>
