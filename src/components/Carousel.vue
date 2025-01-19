ce1ba41d3558210f4b9aef2e5a636b5fa0024309
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-19 16:45:16 -0300

    Add Swiper.js for carousel functionality and styling
    - Integrated Swiper.js version 11.2.1 for enhanced carousel features in the application.
    - Created a new Carousel.vue component to utilize Swiper for displaying items in a responsive slider format.
    - Added custom CSS for Swiper styling, including scrollbar customization and slide appearance.
    - Updated package.json and pnpm-lock.yaml to include Swiper as a dependency, ensuring proper versioning and compatibility.

---
+++ src/components/Carousel.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import '@/assets/css/swipper.css'
+import { Navigation, Pagination } from 'swiper/modules';
+
+export interface Props {
+  items: Array<any>
+}
+
+const props = defineProps<Props>()
+
+const modules = [Pagination, Navigation]
+
+const navigation = {
+  prevEl: '#prev-slide',
+  nextEl: '#next-slide',
+}
+
+const pagination = {
+  el: '.swiper-pagination',
+  clickable: true,
+  renderBullet() {
+    return '<span class="swiper-pagination-bullet"></span>'
+  },
+}
+
+const desktopSlidersPerView
+  = props.items.length > 3 ? 3 : props.items.length
+const mobileSlidersPerView
+  = props.items.length > 2 ? 2 : props.items.length
+
+const breakpoints = {
+  640: {
+    slidesPerView: mobileSlidersPerView,
+    spaceBetween: 5,
+  },
+  1024: {
+    slidesPerView: desktopSlidersPerView,
+    spaceBetween: 5,
+  },
+}
+</script>
+
+<template>
+  <Swiper
+    :slides-per-view="1"
+    :loop="true"
+    :navigation="false"
+    :pagination="false"
+    :breakpoints="breakpoints"
+    :modules="modules"
+    class="w-ful relative flex h-auto flex-col items-start sm:max-w-[900px]"
+  >
+    <SwiperSlide
+      v-for="item in items"
+      :key="item.id"
+      class="h-auto w-full"
+    >
+      <slot :slot-props="{ item }" />
+    </SwiperSlide>
+  </Swiper>
+</template>
+
+<style>
+.swiper-pagination {
+  margin-left: 2rem;
+}
+.swiper-pagination .swiper-pagination-bullet {
+  opacity: 1;
+  background-color: #76767630;
+  width: 33px;
+  height: 10px;
+  border-radius: 4px;
+  margin-right: auto;
+  box-shadow: gray 0px 1px 1px 0px;
+}
+.swiper-pagination .swiper-pagination-bullet-active {
+  background-color: black;
+}
+
+@media screen and (max-width: 640px) {
+  .swiper-pagination {
+    margin-left: -1px;
+  }
+}
+</style>
