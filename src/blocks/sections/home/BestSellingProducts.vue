baa2069269c54e82173997bdb042b9e63dcb1063
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-19 04:36:35 -0300

    Add BestSellingProducts component with product data and layout
    - Introduced a new BestSellingProducts component featuring a list of products with their details including name, price, and image.
    - Implemented a responsive grid layout for displaying products, enhancing user experience on various screen sizes.
    - Utilized TypeScript for product data structure, ensuring type safety and clarity in the codebase.

--- src/blocks/sections/home/BestSellingProducts.vue
+++ src/blocks/sections/home/BestSellingProducts.vue
@@ -1,8 +1,56 @@
-<template>
-  <Button
-    class="w-max mx-auto h-max my-auto"
-    label="Best Selling Products"
-    id="best-selling-products-button"
-  />
-</template>
+<script setup lang="ts">
+import katherineMcadoo from "@/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp";
 
+const products: Array<Product> = [
+  {
+    id: "1",
+    name: "Cat Bowl",
+    price: 20.99,
+    imageSrc: katherineMcadoo,
+    isFavorite: false,
+    currency: "USD",
+    altText: "Cat Bowl",
+  },
+  {
+    id: "2",
+    name: "Cat Bowl",
+    price: 49.99,
+    imageSrc: katherineMcadoo,
+    isFavorite: false,
+    currency: "USD",
+    altText: "Cat Bowl",
+    },
+  {
+    id: "3",
+    name: "Cat Bowl",
+    price: 49.99,
+    imageSrc: katherineMcadoo,
+    isFavorite: false,
+    currency: "USD",
+    altText: "Cat Bowl",
+  },
+  {
+    id: "4",
+    name: "Cat Bowl",
+    price: 49.99,
+    imageSrc: katherineMcadoo,
+    isFavorite: false,
+    currency: "USD",
+    altText: "Cat Bowl",
+  },
+];
+</script>
+
+<template>
+  <section class="mx-auto my-8 max-w-[95vw]">
+    <h2 class="text-2xl font-bold text-center mb-8">Best Selling Products</h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+  </section>
+</template>
