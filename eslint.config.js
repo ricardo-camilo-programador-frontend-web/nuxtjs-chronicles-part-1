5a43222e4280243e9928eae72263842114fcac01
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-20 10:58:58 -0300

    Add Prettier and ESLint configuration files for code formatting and linting

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  rules: {
+    curly: ['error', 'all'],
+  },
+  vue: {
+    overrides: {
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: {
+            max: 1,
+          },
+          multiline: {
+            max: 1,
+          },
+        },
+      ],
+      'vue/block-order': [
+        'error',
+        {
+          order: [['script', 'template'], 'style'],
+        },
+      ],
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'DEFINITION',
+            'LIST_RENDERING',
+            'CONDITIONALS',
+            'TWO_WAY_BINDING',
+            'CONTENT',
+            'RENDER_MODIFIERS',
+            'ATTR_DYNAMIC',
+            'ATTR_STATIC',
+            'ATTR_SHORTHAND_BOOL',
+            'OTHER_DIRECTIVES',
+            'EVENTS',
+          ],
+          alphabetical: false,
+        },
+      ],
+      'vue/no-reserved-component-names': 0,
+      'vue/no-mutating-props': 1,
+      'curly': ['error', 'all'],
+      'no-console': 'warn',
+      'regexp/no-super-linear-backtracking': 'off',
+      'regexp/no-misleading-capturing-group': 'off',
+    },
+  },
+  typescript: {
+    overrides: {
+      'ts/array-type': [
+        'error',
+        {
+          default: 'generic',
+        },
+      ],
+      'curly': ['error', 'all'],
+    },
+  },
+  toml: {
+    overrides: {
+      'toml/indent': [
+        'error',
+        2,
+        {
+          subTables: 1,
+          keyValuePairs: 1,
+        },
+      ],
+    },
+  },
+  ignores: ['.gitlab/', 'README.md', '**/*.d.ts', 'nuxt.config.ts'],
+})
