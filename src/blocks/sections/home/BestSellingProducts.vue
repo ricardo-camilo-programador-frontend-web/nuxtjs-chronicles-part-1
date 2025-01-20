0aa76e7a59826f2851adde2c19992146dcd97960
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-20 16:47:55 -0300

    Refactor BestSellingProducts.vue to remove hardcoded product data
    - Eliminated the static product array from the BestSellingProducts component to streamline the code and improve maintainability.
    - This change prepares the component for dynamic data integration, enhancing flexibility for future updates and data management.

--- src/blocks/sections/home/BestSellingProducts.vue
+++ src/blocks/sections/home/BestSellingProducts.vue
@@ -1,83 +1,8 @@
 <script setup lang="ts">
-import katherineMcadoo from '@/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp'
 import { SwiperSlide } from 'swiper/vue'
 
 const { t } = useI18n()
 
-const products: Array<Product> = [
-  {
-    id: 'premium-cat-food-1',
-    name: t('products.premiumCatFood'),
-    price: 24.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.catBowlAltText'),
-  },
-  {
-    id: 'leather-dog-collar',
-    name: t('products.leatherDogCollar'),
-    price: 34.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.dogCollarAltText'),
-  },
-  {
-    id: 'cat-scratching-post',
-    name: t('products.catScratchingPost'),
-    price: 59.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.scratchingPostAltText'),
-  },
-  {
-    id: 'bird-cage-deluxe',
-    name: t('products.birdCageDeluxe'),
-    price: 89.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.birdCageAltText'),
-  },
-  {
-    id: 'hamster-habitat',
-    name: t('products.hamsterHabitat'),
-    price: 45.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.hamsterHabitatAltText'),
-  },
-  {
-    id: 'fish-tank-filter',
-    name: t('products.fishTankFilter'),
-    price: 29.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.fishFilterAltText'),
-  },
-  {
-    id: 'dog-grooming-kit',
-    name: t('products.dogGroomingKit'),
-    price: 49.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.groomingKitAltText'),
-  },
-  {
-    id: 'cat-litter-box',
-    name: t('products.catLitterBox'),
-    price: 39.99,
-    imageSrc: katherineMcadoo,
-    isFavorite: false,
-    currency: t('products.currency'),
-    altText: t('products.litterBoxAltText'),
-  }
-]
 </script>
 
 <template>
