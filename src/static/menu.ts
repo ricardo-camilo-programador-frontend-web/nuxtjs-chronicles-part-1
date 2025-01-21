c81a243af2197484a22253970e5bb694c66458f9
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-21 10:06:09 -0300

    Add menu links to menu.ts for navigation

---
+++ src/static/menu.ts
@@ -0,0 +1,6 @@
+export const menuLinks: Array<MenuLink> = [
+  { path: '/home', translationKey: 'menu.links.home' },
+  { path: '/shop', translationKey: 'menu.links.shop' },
+  { path: '/about', translationKey: 'menu.links.about' },
+  { path: '/contact', translationKey: 'menu.links.contact' },
+]
