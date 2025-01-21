b265de7bdb7ce2d488c746b61de6175133564ea4
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-21 10:06:24 -0300

    Add DesktopMenu component for navigation links

---
+++ src/blocks/layout/DesktopMenu.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex gap-4 relative mx-auto">
+    <NuxtLink
+      v-for="link in menuLinks"
+      :key="link.path"
+      :to="link.path"
+      class="text-lg hover:text-orange-500 border-b border-transparent hover:border-orange-500 transition-all duration-300 ease-in-out"
+    >
+      {{ $t(link.translationKey) }}
+    </NuxtLink>
+  </div>
+</template>
