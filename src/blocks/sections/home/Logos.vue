9add5a6f02517cb5a6c5227149237b484ceb16a0
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-21 21:27:22 -0300

    Replace outdated logo images with new SVG assets in Logos component for improved visual representation. Update layout and styling for better responsiveness and user experience.

--- src/blocks/sections/home/Logos.vue
+++ src/blocks/sections/home/Logos.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import katherineMcadoo from '@/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp'
+import dogFace from '@/assets/images/svg/dog-face.svg'
+import dogHeart from '@/assets/images/svg/dog-heart.svg'
+import dogHouse from '@/assets/images/svg/dog-house.svg'
+import pawHeart from '@/assets/images/svg/paw-heart.svg'
+import paw from '@/assets/images/svg/paw.svg'
 import { SwiperSlide } from 'swiper/vue'
 
 interface LogoItem {
@@ -15,31 +19,31 @@ const logos: Array<LogoItem> = [
   {
     id: 'logo1',
     name: t('logos.petCare'),
-    imagePath: katherineMcadoo,
+    imagePath: dogFace,
     altText: t('logos.petCareAltText'),
   },
   {
     id: 'logo2',
     name: t('logos.petHouse'),
-    imagePath: katherineMcadoo,
+    imagePath: dogHouse,
     altText: t('logos.petHouseAltText'),
   },
   {
     id: 'logo3',
     name: t('logos.pawLove'),
-    imagePath: katherineMcadoo,
+    imagePath: paw,
     altText: t('logos.pawLoveAltText'),
   },
   {
     id: 'logo4',
     name: t('logos.petServices'),
-    imagePath: katherineMcadoo,
+    imagePath: pawHeart,
     altText: t('logos.petServicesAltText'),
   },
   {
     id: 'logo5',
     name: t('logos.petCareHeart'),
-    imagePath: katherineMcadoo,
+    imagePath: dogHeart,
     altText: t('logos.petCareHeartAltText'),
   },
 ]
@@ -59,18 +63,20 @@ const logos: Array<LogoItem> = [
         <SwiperSlide
           v-for="logo in logos"
           :key="logo.id"
-          class="mx-auto w-full max-w-[17rem] transition-transform hover:scale-105 md:max-w-[200px]"
+          auto-play="1000"
+          class="mx-auto w-full max-w-[17rem] transition-transform hover:scale-105 md:max-w-[200px] h-[35rem] flex flex-col items-center justify-center"
         >
           <Image
             :src="logo.imagePath"
             :alt="logo.altText"
             :title="logo.name"
+            class="w-full h-[8rem] object-cover overflow-visible"
+            loading="lazy"
             width="200"
             height="200"
-            class="h-auto w-full object-contain"
-            loading="lazy"
           />
-          <p class="mt-2 text-center font-medium text-orange-500">
+
+          <p class="text-center font-medium text-orange-500 mt-auto bottom-0 h-6">
             {{ logo.name }}
           </p>
         </SwiperSlide>
