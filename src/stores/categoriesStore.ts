e4614961aa8fe50ee3be5717e02bb06114b96e6e
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-22 10:36:52 -0300

    Refactor categoriesStore to remove hardcoded category data
    - Eliminated the initializeCategories action and its associated hardcoded category data from categoriesStore.
    - This change streamlines the store's structure, promoting a cleaner and more maintainable codebase.
    - Future category data can now be managed externally, enhancing flexibility and scalability.

--- src/stores/categoriesStore.ts
+++ src/stores/categoriesStore.ts
@@ -54,44 +54,7 @@ export const useCategoriesStore = defineStore({
       return this.categories
     },
   },
-  actions: {
-    initializeCategories() {
-      this.categories = [
-        {
-          id: 'accessories',
-          title: 'categories.items.accessories.title',
-          productCount: 84,
-          gradientClass: 'bg-gradient-to-r from-purple-400 to-pink-400',
-          src: backgroundGradientOrange,
-          altText: 'categories.items.accessories.altText',
-        },
-        {
-          id: 'food',
-          title: 'categories.items.food.title',
-          productCount: 64,
-          gradientClass: 'bg-gradient-to-r from-green-400 to-blue-400',
-          src: backgroundGradientOrangeMini,
-          altText: 'categories.items.food.altText',
-        },
-        {
-          id: 'furniture',
-          title: 'categories.items.furniture.title',
-          productCount: 22,
-          gradientClass: 'bg-gradient-to-r from-blue-400 to-teal-400',
-          src: charlesdeluvio,
-          altText: 'categories.items.furniture.altText',
-        },
-        {
-          id: 'bags',
-          title: 'categories.items.bags.title',
-          productCount: 16,
-          gradientClass: 'bg-gradient-to-r from-pink-400 to-red-400',
-          src: katherineMcadoo,
-          altText: 'categories.items.bags.altText',
-        },
-      ]
-    },
-  },
+  actions: {},
 })
 
 acceptHMRUpdate(useCategoriesStore, import.meta.hot)
