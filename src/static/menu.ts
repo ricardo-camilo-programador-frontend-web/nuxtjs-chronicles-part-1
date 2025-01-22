430243ab4f04ea6f0cb4eeaa8427b949eb22751f
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-21 21:48:35 -0300

    Update menu links to use root path for home navigation

--- src/static/menu.ts
+++ src/static/menu.ts
@@ -1,5 +1,5 @@
 export const menuLinks: Array<MenuLink> = [
-  { path: '/home', translationKey: 'menu.links.home' },
+  { path: '/', translationKey: 'menu.links.home' },
   { path: '/shop', translationKey: 'menu.links.shop' },
   { path: '/about', translationKey: 'menu.links.about' },
   { path: '/contact', translationKey: 'menu.links.contact' },
