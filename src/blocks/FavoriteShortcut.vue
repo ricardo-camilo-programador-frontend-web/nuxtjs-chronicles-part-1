c5acd683ca0ad433d24957751d6053adde8dee47
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-23 15:05:15 -0300

    Add FavoriteShortcut component for managing product favorites

---
+++ src/blocks/FavoriteShortcut.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useProductStore } from '@/stores/productStore'
+
+interface FavoriteShortcutProps {
+  product: Product
+}
+
+const props = defineProps<FavoriteShortcutProps>()
+const productStore = useProductStore()
+const { updateProductFavorite } = productStore
+
+function handleFavoriteClick(event: MouseEvent) {
+  event.stopPropagation()
+  updateProductFavorite(props.product)
+}
+</script>
+
+<template>
+  <div>
+    <Button
+      :id="`favorite-button-${product.id}`"
+      :icon="product.favorite ? 'mdi:heart' : 'mdi:heart-outline'"
+      :icon-style="`text-3xl ${product.favorite ? 'text-red-500' : ''}`"
+      class="w-auto py-0"
+      @click="handleFavoriteClick"
+    />
+  </div>
+</template>
