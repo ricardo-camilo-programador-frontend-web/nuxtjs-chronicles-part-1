68891af0732f63a8f624b872c460a7dd3d046eb2
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-23 20:41:45 -0300

    Create FavoriteCard component for displaying favorite products
    - Implemented new Vue component for rendering favorite products
    - Added support for dynamic product display with internationalization
    - Included empty state handling for when no favorites exist
    - Utilized Transition component for smooth rendering

---
+++ src/components/FavoriteCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+interface FavoriteCardProps {
+  favoriteProducts: Array<Product>
+}
+
+defineProps<FavoriteCardProps>()
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="h-[20rem]">
+    <Transition>
+      <div
+        v-if="favoriteProducts.length > 0"
+        class="absolute top-16 right-16 z-[99] h-full w-full"
+      >
+        <div
+          class="absolute right-0 flex h-full w-[17rem] flex-col gap-2 overflow-hidden overflow-y-auto rounded-xl border border-gray-200 bg-white p-2"
+        >
+          <div
+            v-for="product in favoriteProducts"
+            :key="product.id"
+            class="mx-auto flex w-full items-center justify-start gap-2 border-b pb-2"
+          >
+            <Image
+              :src="product.imageSrc"
+              :alt="product.altText"
+              width="50"
+              height="50"
+            />
+
+            <p class="line-clamp-2 text-sm">
+              {{ t(product.name) }}
+            </p>
+
+            <slot :product="product" />
+          </div>
+        </div>
+      </div>
+    </Transition>
+
+    <div
+      v-if="!favoriteProducts.length"
+      class="absolute top-16 right-0 z-[99] flex h-full w-[17rem] items-center justify-center bg-black/50"
+    >
+      <p class="text-white">
+        {{ $t('favorites.empty') }}
+      </p>
+    </div>
+  </div>
+</template>
