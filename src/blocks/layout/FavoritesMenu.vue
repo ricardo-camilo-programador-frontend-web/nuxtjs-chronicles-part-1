bab84c1cd0cd1d79b3b52718b8c4c52513414fe0
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-23 20:41:52 -0300

    Refactor FavoritesMenu to use FavoriteCard component
    - Replaced inline favorite products rendering with new FavoriteCard component
    - Simplified template structure and improved component composition
    - Utilized slot prop for rendering FavoriteShortcut within FavoriteCard
    - Removed redundant menu styling and transition logic

--- src/blocks/layout/FavoritesMenu.vue
+++ src/blocks/layout/FavoritesMenu.vue
@@ -45,52 +45,10 @@ onUnmounted(() => {
       />
     </div>
 
-    <Transition>
-      <div
-        v-if="showFavoritesMenu"
-        ref="favoritesMenu"
-        class="h-[20rem]"
-      >
-        <div
-          v-if="showFavoritesMenu && favoriteProducts.length > 0"
-          class="absolute top-16 right-16 z-[99] h-full w-full"
-        >
-          <div
-            class="absolute right-0 flex h-full w-[17rem] flex-col gap-2 overflow-hidden overflow-y-auto rounded-xl border border-gray-200 bg-white p-2"
-          >
-            <div
-              v-for="product in favoriteProducts"
-              :key="product.id"
-              class="mx-auto flex w-full items-center justify-start gap-2 border-b pb-2"
-            >
-              <Image
-                :src="product.imageSrc"
-                :alt="product.altText"
-                width="50"
-                height="50"
-              />
-
-              <p class="line-clamp-2 text-sm">
-                {{ product.name }}
-              </p>
-
-              <FavoriteShortcut
-                :product="product"
-                @favorite-updated="setFavoriteProducts"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div
-          v-if="showFavoritesMenu && !favoriteProducts.length"
-          class="absolute top-16 right-0 z-[99] flex h-full w-[17rem] items-center justify-center bg-black/50"
-        >
-          <p class="text-white">
-            {{ $t('favorites.empty') }}
-          </p>
-        </div>
-      </div>
-    </Transition>
+    <FavoriteCard :favorite-products="favoriteProducts">
+      <template #default="{ product }">
+        <FavoriteShortcut :product="product" />
+      </template>
+    </FavoriteCard>
   </div>
 </template>
