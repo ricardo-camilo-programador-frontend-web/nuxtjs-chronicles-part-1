f55d0527499ce243f2dbab704e75f4c8c006a54a
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-01-23 14:10:40 -0300

    Refactor FavoritesMenu component to improve favorites display logic and styling
    - Updated the FavoritesMenu.vue to conditionally render the favorites list and an empty state message based on the presence of favorite products.
    - Enhanced the layout and styling for better visual consistency, including adjustments to positioning and class properties.
    - These changes improve user experience by providing clearer feedback when no favorites are available and ensuring a polished appearance.

--- src/blocks/layout/FavoritesMenu.vue
+++ src/blocks/layout/FavoritesMenu.vue
@@ -51,47 +51,49 @@ onUnmounted(() => {
     </div>
 
     <Transition>
-      <div
-        v-if="showFavoritesMenu"
-        class="absolute right-16 top-16 z-[99] h-full w-full"
-      >
+      <div v-if="showFavoritesMenu">
         <div
-          ref="favoritesMenu"
-          class="absolute right-0 flex max-h-[20rem] w-[17rem] flex-col gap-2 overflow-hidden overflow-y-auto rounded-xl bg-white p-2 border border-gray-200"
+          v-if="showFavoritesMenu && favoriteProducts.length > 0"
+          class="absolute right-16 top-16 z-[99] h-full w-full"
         >
           <div
-            v-for="product in favoriteProducts"
-            :key="product.id"
-            class="mx-auto flex w-full items-center justify-start gap-2 border-b pb-2"
+            ref="favoritesMenu"
+            class="absolute right-0 flex max-h-[20rem] w-[17rem] flex-col gap-2 overflow-hidden overflow-y-auto rounded-xl bg-white p-2 border border-gray-200"
           >
-            <Image
-              :src="product.imageSrc"
-              :alt="product.altText"
-              width="50"
-              height="50"
-            />
+            <div
+              v-for="product in favoriteProducts"
+              :key="product.id"
+              class="mx-auto flex w-full items-center justify-start gap-2 border-b pb-2"
+            >
+              <Image
+                :src="product.imageSrc"
+                :alt="product.altText"
+                width="50"
+                height="50"
+              />
 
-            <p class="line-clamp-2 text-sm">
-              {{ product.name }}
-            </p>
+              <p class="line-clamp-2 text-sm">
+                {{ product.name }}
+              </p>
 
-            <Button
-              :id="`favorite-button-${product.id}`"
-              :icon="product.favorite ? 'mdi:heart' : 'mdi:heart-outline'"
-              :icon-style="`text-3xl ${product.favorite ? 'text-red-500' : ''}`"
-              class="w-auto py-0"
-            />
+              <Button
+                :id="`favorite-button-${product.id}`"
+                :icon="product.favorite ? 'mdi:heart' : 'mdi:heart-outline'"
+                :icon-style="`text-3xl ${product.favorite ? 'text-red-500' : ''}`"
+                class="w-auto py-0"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div
-        v-else
-        class="absolute right-0 top-16 z-[99] h-full w-full bg-black/50"
-      >
-        <p class="text-black">
-          {{ $t('favorites.empty') }}
-        </p>
+        <div
+          v-if="showFavoritesMenu && !favoriteProducts.length"
+          class="absolute right-0 top-16 z-[99] flex h-full items-center justify-center bg-black/50 w-[17rem]"
+        >
+          <p class="text-white">
+            {{ $t('favorites.empty') }}
+          </p>
+        </div>
       </div>
     </Transition>
   </div>
