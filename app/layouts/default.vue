f832b59452c2add9b659492551da49dc5bac07f9
ricardo.camilo.dev <ricardo_hks@hotmail.com>
2025-12-22 17:59:52 -0300

    fix: add title attribute for Google Tag Manager in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -32,6 +32,7 @@ useHead({
         height="0"
         width="0"
         style="display: none; visibility: hidden"
+        title="Google Tag Manager"
         />
       </noscript>
     </ClientOnly>
