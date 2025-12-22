0c87d22a616ed47733aed89fab10b51ce8d6dc54
ricardo.camilo.dev <ricardo_hks@hotmail.com>
2025-12-22 20:20:03 -0300

    :recycle: refactor: use export...from syntax for image re-exports

--- app/utils/imageFiles.ts
+++ app/utils/imageFiles.ts
@@ -1,37 +1,17 @@
-import backgroundGradientOrange from '~/assets/images/background-gradient-orange.webp'
-import backgroundGradientOrangeMini from '~/assets/images/background-gradient-orange-mini.webp'
-import birdCage from '~/assets/images/bird-cage.webp'
-import catFood from '~/assets/images/cat-food.webp'
-import catFoodPremium from '~/assets/images/cat-food-premium.webp'
-import charlesdeluvio from '~/assets/images/charlesdeluvio-5R5HgLyHMVQ-unsplash.webp'
-import dogCollar from '~/assets/images/dog-collar.webp'
-import dogFoodLarge from '~/assets/images/dog-food-large.webp'
-import dogFoodMedium from '~/assets/images/dog-food-medium.webp'
-import dogFoodMini from '~/assets/images/dog-food-mini.webp'
-import dogFoodOrange from '~/assets/images/dog-food-orange.webp'
-import fishFilter from '~/assets/images/fish-filter.webp'
-import groomingKit from '~/assets/images/grooming-kit.webp'
-import hamsterHabitat from '~/assets/images/hamster-habitat.webp'
-import katherineMcadoo from '~/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp'
-import litterBox from '~/assets/images/litter-box.webp'
-import scratchingPost from '~/assets/images/scratching-post.webp'
-
-export {
-  backgroundGradientOrange,
-  backgroundGradientOrangeMini,
-  birdCage,
-  catFood,
-  catFoodPremium,
-  charlesdeluvio,
-  dogCollar,
-  dogFoodLarge,
-  dogFoodMedium,
-  dogFoodMini,
-  dogFoodOrange,
-  fishFilter,
-  groomingKit,
-  hamsterHabitat,
-  katherineMcadoo,
-  litterBox,
-  scratchingPost,
-}
+export { default as backgroundGradientOrange } from '~/assets/images/background-gradient-orange.webp'
+export { default as backgroundGradientOrangeMini } from '~/assets/images/background-gradient-orange-mini.webp'
+export { default as birdCage } from '~/assets/images/bird-cage.webp'
+export { default as catFood } from '~/assets/images/cat-food.webp'
+export { default as catFoodPremium } from '~/assets/images/cat-food-premium.webp'
+export { default as charlesdeluvio } from '~/assets/images/charlesdeluvio-5R5HgLyHMVQ-unsplash.webp'
+export { default as dogCollar } from '~/assets/images/dog-collar.webp'
+export { default as dogFoodLarge } from '~/assets/images/dog-food-large.webp'
+export { default as dogFoodMedium } from '~/assets/images/dog-food-medium.webp'
+export { default as dogFoodMini } from '~/assets/images/dog-food-mini.webp'
+export { default as dogFoodOrange } from '~/assets/images/dog-food-orange.webp'
+export { default as fishFilter } from '~/assets/images/fish-filter.webp'
+export { default as groomingKit } from '~/assets/images/grooming-kit.webp'
+export { default as hamsterHabitat } from '~/assets/images/hamster-habitat.webp'
+export { default as katherineMcadoo } from '~/assets/images/katherine-mcadoo-vSS2_KfzbLY-unsplash.webp'
+export { default as litterBox } from '~/assets/images/litter-box.webp'
+export { default as scratchingPost } from '~/assets/images/scratching-post.webp'
