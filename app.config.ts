b0b100f4b3fb2e0e59c70c4f9601fbb7141c630b
ricardo.camilo.dev <ricardo.camilo.dev@gmail.com>
2025-12-29 17:48:55 -0300

    :sparkles: chore: enhance app configuration with additional modules and server bundle settings
    - Add '@nuxt/icon' module to app configuration for improved icon management
    - Introduce serverBundle collections for expanded icon support in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,12 @@
 import { defineAppConfig } from '#imports'
 
 export default defineAppConfig({
+  modules: ['@nuxt/icon'],
   icon: {
     mode: 'css',
     cssLayer: 'base',
+  },
+  serverBundle: {
+    collections: ['mdi', 'circle-flags', 'ph', 'simple-icons', 'cib'],
   },
 })
