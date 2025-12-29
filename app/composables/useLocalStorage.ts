07f76a2d3dd0fb84e9a92703b86879caf259ff2f
ricardo.camilo.dev <ricardo.camilo.dev@gmail.com>
2025-12-29 16:04:12 -0300

    :art: refactor: simplify localStorage checks in composables
    - Replace `typeof` checks with direct undefined comparisons in useLocalStorage.ts
    - Improve code readability and maintainability by streamlining localStorage access logic

--- app/composables/useLocalStorage.ts
+++ app/composables/useLocalStorage.ts
@@ -1,17 +1,17 @@
 function saveItemOnLocalStorage(itemName: string, itemValue: string) {
-  if (typeof globalThis.window !== 'undefined') {
+  if (globalThis.window !== undefined) {
     globalThis.window.localStorage.setItem(itemName, itemValue)
   }
 }
 
 function removeItemFromLocalStorage(itemName: string) {
-  if (typeof globalThis.window !== 'undefined') {
+  if (globalThis.window !== undefined) {
     globalThis.window.localStorage.removeItem(itemName)
   }
 }
 
 function getItemFromLocalStorage(itemName: string) {
-  if (typeof globalThis.window !== 'undefined') {
+  if (globalThis.window !== undefined) {
     const itemValue = globalThis.window.localStorage.getItem(itemName)
 
     return itemValue
